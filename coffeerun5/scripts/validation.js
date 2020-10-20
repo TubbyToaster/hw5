377ce67ba632c550d07ac86a0e3e16fd1a249608
TubbyToaster <dylanconnors@csu.fullerton.edu>
2020-10-20 15:20:46 -0700

    Add files via upload

---
+++ coffeerun5/scripts/validation.js
@@ -0,0 +1,17 @@
+(function (window) {
+  'use strict';
+​
+  var App = window.App || {};
+​
+  var Validation = {
+      isCompanyEmail: function(email) {
+          return /.+@what\.com$/.test(email);
+      },
+      isHighCaffeine: function(caffeine) {
+          return caffeine > 50;
+      }
+  };
+  App.Validation = Validation;
+  window.App = App;
+  
+})(window);
