614c6df609f18010abed57723523d28687abb16f
TubbyToaster <dylanconnors@csu.fullerton.edu>
2020-10-22 01:01:14 -0700

    Update checklist.js

--- coffeerun5/scripts/checklist.js
+++ coffeerun5/scripts/checklist.js
@@ -1,57 +1,69 @@
-(function(window) {
-  'use strict';
-  var App = window.App || {};
-  var $ = window.jQuery;
-  function CheckList(selector) {
-    if (!selector) {
-      throw new Error('No selector provided');
-    }
-    this.$element = $(selector);
-    if (this.$element.length === 0) {
-      throw new Error('Could not find element with selector: ' + selector);
-    }
-  }
-  CheckList.prototype.addRow = function(coffeeOrder) {
-    this.removeRow(coffeeOrder.email);
-    var rowElement = new Row(coffeeOrder);
-    this.$element.append(rowElement.$element);
-  };
-  CheckList.prototype.removeRow = function(email) {
-    this.$element
-      .find('[value="' + email + '"]')
-      .closest('[data-coffee-order="checkbox"]')
-      .empty();
-  };
-  CheckList.prototype.addClickHandler = function(fn) {
-    this.$element.on('click', 'input', function(event) {
-      this.removeRow(event.target.value);
-      fn(event.target.value);
-    }.bind(this));
-  };
-  function Row(coffeeOrder) {
-    var $div = $('<div/>', {
-      'data-coffee-order': 'checkbox',
-      class: 'checkbox'
-    });
-    var $label = $('<label></label>');
-    var $checkbox = $('<input></input>', {
-      type: 'checkbox',
-      value: coffeeOrder.email
-    });
-    var description = coffeeOrder.size + ' ';
-    if (coffeeOrder.flavor) {
-      description += coffeeOrder.flavor + ' ';
-    }
-    description += coffeeOrder.coffee + ', ';
-    description += ' (' + coffeeOrder.email + ')';
-    description += ' [' + coffeeOrder.strength + 'x]';
-    $label.append($checkbox);
-    $label.append(description);
-    $div.append($label);
+(function (window) {
+    'use strict';
 
-    this.$element = $div;
-  }
+    var App = window.App || {};
+    var $ = window.jQuery;
 
-  App.CheckList = CheckList;
-  window.App = App;
+    class CheckList {
+        constructor(selector) {
+            if (!selector) {
+                throw new Error('No selector provided');
+            }
+          this.$element = $(selector);
+          if (this.$element.length === 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+          }
+        }
+        addClickHandler(fn) {
+            this.$element.on('click', 'input', function(event) {
+                var email = event.target.value;
+                fn(email)
+                    .then(function() {
+                        this.removeRow(email);
+                    }.bind(this));
+                }.bind(this));
+        }
+        addRow(coffeeOrder) {
+            this.removeRow(coffeeOrder.emailAddress);
+
+            var rowElement = new Row(coffeeOrder);
+            this.$element.append(rowElement.$element);
+        }
+        removeRow(email) { 
+            this.$element.find('[value="' + email + '"]')
+                          .closest('[data-coffee-order="checkbox"]')
+                          .remove();
+        }
+    }
+    class Row {
+        constructor(coffeeOrder) {    
+            var $div = $('<div></div>', { 
+                'data-coffee-order': 'checkbox',  'class': 'checkbox'    
+            });
+
+            var $label = $('<label></label>');
+
+            var $checkbox = $('<input></input>', {
+                type: 'checkbox',
+                value: coffeeOrder.emailAddress
+              });
+
+            var description = coffeeOrder.size + ' ';
+            if (coffeeOrder.flavor) {
+                description += coffeeOrder.flavor + ' ';
+            }
+            description += coffeeOrder.coffee + ', ';
+            description += ` (${coffeeOrder.emailAddress})`;
+            description += ` [${coffeeOrder.strength}x]`;
+
+            $label.append($checkbox);
+            $label.append(description);
+            $div.append($label);
+            this.$element = $div;
+        }
+    }
+    
+    App.CheckList = CheckList;
+    window.App = App;
+    
 })(window);
